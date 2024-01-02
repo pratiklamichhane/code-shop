29a2fd697c55ba285da38d7213e4cab43fde15ec
Pratik Lamichhane <lamichhanepratik9@gmail.com>
2024-01-02 15:18:20 +0545

    added blank login and signup page

--- ecom/accounts/urls.py
+++ ecom/accounts/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('login/' , views.login_page , name="login" ),
+    path('signup/' , views.signup_page , name="signup"),
 ]
+
--- ecom/accounts/views.py
+++ ecom/accounts/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def login_page(request):
     return render(request, 'accounts/login.html')
+
+def signup_page(request):
+    return render(request , 'accounts/signup.html')
