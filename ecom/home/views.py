d63eaa125aff3bcb687089fe6d7429a4bf48ee49
Pratik Lamichhane <lamichhanepratik9@gmail.com>
2024-01-02 19:09:12 +0545

    enhanced signin and login

--- ecom/home/views.py
+++ ecom/home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request , "home/index.html")
+
+def home(request):
+    return render(request , "home/base.html", {'user':request.user})
